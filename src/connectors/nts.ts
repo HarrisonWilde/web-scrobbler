f6aed4d488ae291780e5de76d19b1b8ffdf2ccbd
HarrisonWilde <harrisondwilde@outlook.com>
2024-02-04 21:21:46 +0000

    Initial commit

--- src/connectors/nts.ts
+++ src/connectors/nts.ts
@@ -1,16 +1,74 @@
 export {};
 
-Connector.playerSelector = '.page-live-tracks__body';
+Connector.playerSelector = '#react-root';
 
-Connector.artistSelector =
-	'.page-live-tracks__list li:first-child .page-live-tracks__artist-title';
+document.body.insertAdjacentHTML(
+	'beforeend',
+	'<div id="nts-live-tracklist" style="display: none;"></div>',
+);
 
-Connector.trackSelector =
-	'.page-live-tracks__list li:first-child .page-live-tracks__song-title';
+let live: boolean | null = null;
+let artist: string | null = null;
+let track: string | null = null;
 
-Connector.scrobbleInfoLocationSelector = '.page-live-tracks__title';
+Connector.isPlaying = () => {
+	live = document.querySelector('.live-channel--playing') !== null;
+	if (live) {
+		return true;
+	}
+	const showPlaying =
+		document.querySelector('.soundcloud-player__play-icon title')
+			?.textContent !== 'Play';
+	if (showPlaying) {
+		return true;
+	}
+	return false;
+};
+
+function loadTracklist() {
+	fetch(
+		'https://mysterious-journey-91752-edf28ad9f51e.herokuapp.com/https://nts.live/live-tracklist/1',
+	)
+		.then((res) => res.text())
+		.then((text) => {
+			const element = document.getElementById('nts-live-tracklist');
+			if (element) {
+				element.innerHTML = text;
+			}
+		});
+}
+
+Connector.getArtistTrack = () => {
+	if (live) {
+		artist = document.querySelector(
+			'.page-live-tracks__list li:first-child .page-live-tracks__artist-title',
+		)?.textContent;
+		track = document.querySelector(
+			'.page-live-tracks__list li:first-child .page-live-tracks__song-title',
+		)?.textContent;
+	} else {
+		const artistElement = document.querySelector(
+			'.episode-player-tracklist__track--is-playing .episode-player-tracklist__artist',
+		);
+		if (artistElement) {
+			artist = artistElement.textContent;
+		}
+		const trackElement = document.querySelector(
+			'.episode-player-tracklist__track--is-playing .episode-player-tracklist__title',
+		);
+		if (trackElement) {
+			track = trackElement.textContent;
+		}
+	}
+	return { artist, track };
+};
+
+Connector.scrobbleInfoLocationSelector = live
+	? '.live-header__live-now-label'
+	: '.soundcloud-player__content';
+
 Connector.scrobbleInfoStyle = {
 	...Connector.scrobbleInfoStyle,
-	fontSize: '0.7em',
-	marginTop: '0.7em',
+	fontSize: '0.5em',
+	marginLeft: '2em',
 };
